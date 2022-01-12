5f5946dcb4bd795c5990f9388da83198a1f7eab1
omer <sanli.omer@metu.edu.tr>
2022-01-12 13:16:15 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by omer sanli on 12.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
